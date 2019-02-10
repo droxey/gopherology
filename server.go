85d2d886c3feea9c749477c3dcbc9bda1be7ef22
Dani Roxberry <dani@bitoriented.com>
2019-02-10 13:04:19 -0800

    [test] mv

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/droxey/gopherology/utils"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"gopkg.in/go-playground/validator.v9"
@@ -47,35 +48,10 @@ func main() {
 			return
 		}
 
-		pq.Path = CalculateLifePath(pq.Day, pq.Month, pq.Year)
+		pq.Path = utils.CalculateLifePath(pq.Day, pq.Month, pq.Year)
 		pq.Message = "Your Life Path Number is " + fmt.Sprint(pq.Path)
 		return c.JSONPretty(http.StatusOK, pq, "  ")
 	})
 
 	e.Logger.Fatal(e.Start(":1234"))
-}
-
-func sum(i uint64) (sum int) {
-	b64 := uint64(10)
-	for ; i > 0; i /= b64 {
-		sum += int(i % b64)
-	}
-	return
-}
-func total(i int) (total int) {
-	if i > 9 && i != 11 {
-		total = sum(uint64(i))
-	} else {
-		total = i
-	}
-	return
-}
-
-// CalculateLifePath sums each individual date portion, then
-// sums again if a date portion is greater than 9 and does not equal 11.
-func CalculateLifePath(d uint64, m uint64, y uint64) int {
-	day := total(sum(d))
-	month := total(sum(m))
-	year := total(sum(y))
-	return day + month + year
 }
---
+++ utils/main.go
@@ -0,0 +1,26 @@
+package main
+
+func sum(i uint64) (sum int) {
+	b64 := uint64(10)
+	for ; i > 0; i /= b64 {
+		sum += int(i % b64)
+	}
+	return
+}
+func total(i int) (total int) {
+	if i > 9 && i != 11 {
+		total = sum(uint64(i))
+	} else {
+		total = i
+	}
+	return
+}
+
+// CalculateLifePath sums each individual date portion, then
+// sums again if a date portion is greater than 9 and does not equal 11.
+func CalculateLifePath(d uint64, m uint64, y uint64) int {
+	day := total(sum(d))
+	month := total(sum(m))
+	year := total(sum(y))
+	return day + month + year
+}
