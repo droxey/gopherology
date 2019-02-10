a4fb97f39b0150149644d34c333f9171532ec622
Dani Roxberry <dani@bitoriented.com>
2019-02-10 12:38:29 -0800

    [add] finished product

--- README.md
+++ README.md
@@ -6,21 +6,14 @@
 
 ```bash
 $ go run server.go
-   ____    __
-  / __/___/ /  ___
- / _// __/ _ \/ _ \
-/___/\__/_//_/\___/ v3.3.dev
-High performance, minimalist Go web framework
-https://echo.labstack.com
-____________________________________O/_______
-                                    O\
-⇨ http server started on [::]:9999
 ```
 
 ## Call API
 
 ```bash
-$ curl -X POST http://localhost:9999/path \
+$ curl -X POST http://localhost:1234/path \
   -H 'Content-Type: application/json' \
   -d '{"day":26,"month":6,"year":1988}'
+
+22
 ```
--- server.go
+++ server.go
@@ -5,20 +5,25 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/mattn/echo-livereload"
 	"gopkg.in/go-playground/validator.v9"
 )
 
 type (
 	// PathQuery represents a user date submission.
 	PathQuery struct {
-		Year  int `json:"year" validate:"required"`
-		Month int `json:"month" validate:"required"`
-		Day   int `json:"day" validate:"required"`
+		Year  uint64 `json:"year" validate:"required"`
+		Month uint64 `json:"month" validate:"required"`
+		Day   uint64 `json:"day" validate:"required"`
 	}
 
 	// PathQueryValidator ensures date inputs are valid.
 	PathQueryValidator struct {
 		validator *validator.Validate
+	}
+
+	Message struct {
+		Path int `json:"path"`
 	}
 )
 
@@ -30,10 +35,7 @@ func main() {
 	e.Use(middleware.RequestID()) // https://echo.labstack.com/middleware/request-id
 	e.Use(middleware.Logger())    // https://echo.labstack.com/middleware/logger
 	e.Use(middleware.Recover())   // https://echo.labstack.com/middleware/recover
-	e.Use(middleware.AddTrailingSlashWithConfig(middleware.TrailingSlashConfig{
-		// https://echo.labstack.com/middleware/trailing-slash
-		RedirectCode: http.StatusMovedPermanently,
-	}))
+	e.Use(livereload.LiveReload())
 
 	// Configure validation for PathQuery.
 	// https://echo.labstack.com/guide/request#validate-data
@@ -41,20 +43,56 @@ func main() {
 
 	// POST /path
 	e.POST("/path", func(c echo.Context) (err error) {
-		pq := new(PathQuery)
+		pq := &PathQuery{}
 		if err = c.Bind(pq); err != nil {
 			return
 		}
 		if err = c.Validate(pq); err != nil {
 			return
 		}
-		return c.JSONPretty(http.StatusOK, pq, "  ")
+
+		final := CalculateLifePath(pq.Day, pq.Month, pq.Year)
+		return c.JSON(http.StatusOK, final)
 	})
-	e.Logger.Fatal(e.Start(":9999"))
+
+	e.Logger.Fatal(e.Start(":1234"))
 }
 
 // Validate ensures data sent to the server is valid,
 // and informs the user of invalide submissions.
 func (cv *PathQueryValidator) Validate(i interface{}) error {
 	return cv.validator.Struct(i)
+}
+
+// Sum adds each digit of a number.
+func sum(i uint64) (sum int) {
+	b64 := uint64(10)
+	for ; i > 0; i /= b64 {
+		sum += int(i % b64)
+	}
+	return
+}
+
+func total(i int) (total int) {
+	if i > 9 && i != 11 {
+		total = sum(uint64(i))
+	} else {
+		total = i
+	}
+	return total
+}
+
+func CalculateLifePath(d uint64, m uint64, y uint64) int {
+	// Step 1: Sum each individual date portion.
+	day := sum(d)
+	month := sum(m)
+	year := sum(y)
+
+	// Step 2: Sum again if a date portion is greater than 9 and does not equal 11.
+	day = total(day)
+	month = total(month)
+	year = total(year)
+
+	// Step 3: Add and return
+	return day + month + year
 }
